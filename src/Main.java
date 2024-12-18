ca984f0f3e986f950f9d285162488b49bbb7685d
VladTca <votsats@gmail.com>
2024-12-18 15:14:13 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double originalPrice = 100.00;
+        double rateTax = 19.00;
+        System.out.println("priceWithTax = "+(originalPrice+originalPrice*rateTax/100)+" евро");
+    }
+}
